a99f9aa155085f9768881b2e6418ff87bf543100
wangpeng <2305896706@qq.com>
2023-03-30 15:20:44 +0800

    麒麟踏祥云，feat:init ddd project

---
+++ chatbot-api-domain/src/main/java/com/wp/chatbot/api/domain/package-info.java
@@ -0,0 +1,1 @@
+package com.wp.chatbot.api.domain;
---
+++ chatbot-api-interfaces/src/main/java/com/wp/chatbot/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.wp.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动器
+ * @author 麒麟踏祥云
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class);
+    }
+}
